ad8f4bef345f94048b89bf7349e84c5c8db0d9e6
ahmedeisa22 <70604321+ahmedeisa22@users.noreply.github.com>
2022-04-03 13:41:11 +0200

    Update For-loop-challenge.py

--- Linkedin/Byond Fundamental/For-loop-challenge.py
+++ Linkedin/Byond Fundamental/For-loop-challenge.py
@@ -1,1 +1,12 @@
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
 
+print("Our gruit selection")
+for name in fruits:
+    print(name)
