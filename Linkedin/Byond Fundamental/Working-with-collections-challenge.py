fcb09f65ed6dc1949b81e412c5ae907df30d4545
ahmedeisa22 <70604321+ahmedeisa22@users.noreply.github.com>
2022-04-03 13:43:57 +0200

    Update Working-with-collections-challenge.py

--- Linkedin/Byond Fundamental/Working-with-collections-challenge.py
+++ Linkedin/Byond Fundamental/Working-with-collections-challenge.py
@@ -1,1 +1,33 @@
-v
+# star1 = 'Sol'
+# star2 = 'Alpha centaural'
+# star3 = 'Barrand'
+# star4 = 'Wolf 359'
+
+stars = [
+    'Sol',
+    'Alpha centaural',
+    'Barrand',
+    'Wolf 359'
+]
+
+print(stars[3])
+
+# Africian = 'Kilomanjro'
+# Antractic = 'Vinson'
+# Australian = 'punck Jay'
+# Eurason = 'Everset'
+# North_Amercian = 'Denali'
+# Pacific = 'Muna kea'
+# South_Amercian = 'Accongua'
+
+peaks = {
+    'Africian':'Kilomanjro',
+    'Antractic': 'Vinson',
+    'Australian': 'punck Jay',
+    'Eurason':  'Everset',
+    'North_Amercian': 'Denali',
+    'Pacific': 'Muna kea',
+    'South_Amercian': 'Accongua'
+}
+
+print(peaks['Pacific'])
